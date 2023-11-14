8b3d77eaba29cba3266af5e60623e9f9839ec1bf
DeldMi <andre88444@gmail.com>
2023-11-14 14:57:08 -0300

    05-04

--- myApp/src/pages/rxjs-page.component.js
+++ myApp/src/pages/rxjs-page.component.js
@@ -1,13 +1,12 @@
 
 import { Component } from '@angular/core';
-import { Subscription, interval, of } from 'rxjs';
+import { Subscription, filter, interval, map, of, range } from 'rxjs';
 
 
 @Component({
     selector: 'app-rxjs',
     templateUrl: "rxjs-page.component.html"
 })
-
 export class RXJSPageComponent implements OnInit {
 
     items = [];
@@ -16,6 +15,7 @@ export class RXJSPageComponent implements OnInit {
 
     // observable = of('itms 1');
     observable = interval(1000);
+    observable2 = range(1,10);
 
     constructor(){};
 
@@ -25,9 +25,20 @@ export class RXJSPageComponent implements OnInit {
     }
 
     nrOnInit(){
-        this.subscription = this.observable.subscribe((item)=>{
+        // this.subscription = this.observable
+        // .pipe(
+        //     map( x => x * 2),
+        //     tap( x => console.log(x)),
+        //     filter(x=> x < 10)
+        // )
+        // .subscribe((item)=>{
+        //     this.items.push(item);
+        // })
+
+
+        this.observable2.subscribe(item=>{
             this.items.push(item);
-        })
+        });
     };
 
     desinscrever(){
